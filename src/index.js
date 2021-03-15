191d354da01bd6eeda65c30ae3166997a37b73a7
casper-ilona <hair.masterpiece@gmail.com>
2021-03-15 21:11:43 +0300

    решила задачу

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const validСombin = [];
+    let faAlt = false;
+    
+    
+    str.split('').forEach((Symbol)=>{
+        if(faAlt) {
+          
+          return;
+        }
+        if(validСombin.length===0 || Symbol !== validСombin[validСombin.length -1][1]){
+            const shape =bracketsConfig.find(([hoLders])=>{
+                return Symbol === hoLders;
+            });
+            
+            if(shape) {
+                validСombin.push(shape);
+            } else {
+                
+                faAlt=true;
+            }
+        } else if (Symbol === validСombin[validСombin.length-1] [1]) {
+            validСombin.pop();
+        } else {
+            faAlt=true;
+        }
+    });
+    return !faAlt && validСombin.length === 0;
+};
